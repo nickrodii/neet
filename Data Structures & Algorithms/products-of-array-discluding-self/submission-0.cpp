e7693a117699b8cd3f04152bc08402cc2144812d
Nick Rodi <nickjrodi@gmail.com>
2026-08-04 08:20:49 -0700

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> res;
+        for (int i = 0; i < nums.size(); i++) {
+            int temp = 1;
+            for (int j = 0; j < nums.size(); j++) {
+                if (j == i) continue;
+                temp *= nums[j];
+            }
+            res.push_back(temp);
+        }
+
+        return res;
+    }
+};
